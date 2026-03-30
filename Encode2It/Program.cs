d4aafb2b4594a57fe3835d702180509dc15aeaa1
PicelBoi <picelboi@picelboi.xyz>
2026-03-29 22:30:09 -0400

    Test program.

--- Encode2It/Program.cs
+++ Encode2It/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Encode2It.Encoders;
+
+// Attempt to test.
+Console.WriteLine(
+    new Listings()
+    {
+        Listing = [new Listing(), new Listing(), new Listing()]
+    }.Generate()
+);
+Console.WriteLine(new CurrentConditions().Generate());
+Console.WriteLine(new ThreeDayForecast().Generate());
+Console.WriteLine(new EighteenHourForecast().Generate());
