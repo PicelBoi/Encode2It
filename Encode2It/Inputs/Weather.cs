747d250aca9ab1adc1df8e52085fcbd3683ed534
PicelBoi <picelboi@picelboi.xyz>
2026-04-02 18:02:38 -0400

    fix syntax issue

--- Encode2It/Inputs/Weather.cs
+++ Encode2It/Inputs/Weather.cs
@@ -275,7 +275,7 @@ public class WeatherInputs
         try
         {
             HttpClient client = new();
-            string url = api + "/v1/forecast?latitude=40.7143&longitude=-74.006&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code,is_day&current=temperature_2m,weather_code,surface_pressure,wind_speed_10m,wind_direction_10m,is_day&timezone=GMT&timeformat=unixtime&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_hours=12"
+            string url = api + "/v1/forecast?latitude=40.7143&longitude=-74.006&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code,is_day&current=temperature_2m,weather_code,surface_pressure,wind_speed_10m,wind_direction_10m,is_day&timezone=GMT&timeformat=unixtime&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_hours=12";
             if (keyenabled)
             {
                 api += $"&apikey={key}";
