cffc5fd160cdc12e6bf2f8e7e6b06b0a2f792a53
PicelBoi <picelboi@picelboi.xyz>
2026-04-02 16:54:26 -0400

    Add Config class

---
+++ Encode2It/Core/Config.cs
@@ -0,0 +1,34 @@
+using System.Xml.Serialization;
+using Encode2It.Schemas.Core;
+
+namespace Encode2It.Core;
+
+public class Config
+{
+    private Logger logger = new("Config");
+    public ConfigSchema? config = new();
+
+    public Config()
+    {
+        // Check if file exists.
+        if (File.Exists("./config.xml"))
+        {
+            // If so, read it and set config.
+            config = (ConfigSchema?)new XmlSerializer(typeof(ConfigSchema)).Deserialize(File.OpenRead("./config.xml"));
+
+            // Check if config failed to parse.
+            if (config == null)
+            {
+                logger.Error("Failed to parse config! Config must be corrupt! Exiting...");
+                Environment.Exit(1);
+            }
+        }
+        else
+        {
+            // Write config and exit.
+            new XmlSerializer(typeof(ConfigSchema)).Serialize(File.OpenWrite("./config.xml"), config);
+            logger.Error("Config doesn't exist, therefore we created a new config file. Please set all parameters and try again.");
+            Environment.Exit(0);
+        }
+    }
+}
