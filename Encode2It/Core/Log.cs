e5aa5615e30eaa97987353340dbacafd7961cc41
PicelBoi <picelboi@picelboi.xyz>
2026-04-01 22:34:22 -0400

    Make log output stuff

--- Encode2It/Core/Log.cs
+++ Encode2It/Core/Log.cs
@@ -7,12 +7,30 @@ public class Logger
 
     public Logger(string name)
     {
-        name = "Encode2It - " + name;
+        Directory.CreateDirectory("./Logs");
+        Directory.CreateDirectory($"./Logs/{name}");
+        Name = name;
     }
 
-    private static void LogTemplate(string type, string msg)
+    private void LogTemplate(string type, string msg)
     {
-        Console.WriteLine($"[{type}] - {DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()} - {msg}");
+        string message = $"[{type}] - {DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()} - {msg}";
+        string path = $"./Logs/{Name}/{DateTime.Now.ToShortDateString()}";
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(message);
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(message);
+            }
+        }
     }
 
     public void Info(string msg)
